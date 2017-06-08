526cffeebfc498c5340ca3c98366d55e086538fc
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-08 11:03:22 +0800

    update

--- src/lib/syntax/inline/InlineCode.js
+++ src/lib/syntax/inline/InlineCode.js
@@ -1,5 +1,37 @@
 function parse(str, children, renderTree) {
 
+    let result = str.match(/^(`+)/);
+
+    if (!result) {
+        return;
+    }
+
+    let flag = result[1],
+        restStr;
+
+    while (flag.length > 0) {
+
+        restStr = str.slice(flag.length);
+        result = restStr.match(new RegExp(`^(.*?)(${flag})`));
+
+        if (result) {
+            break;
+        }
+
+        flag = flag.slice(1);
+
+    }
+
+    if (!result) {
+        return;
+    }
+
+    return [{
+        display: 'inline',
+        type: 'InlineCode',
+        rawValue: result[1]
+    }, result[1].length + flag.length * 2];
+
 }
 
 function render(data = '', node) {
