4caba4286175f6c766f4bda3b65e097003f19196
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-14 16:32:05 +0800

    bug fixed

--- src/lib/syntax/inline/Link.js
+++ src/lib/syntax/inline/Link.js
@@ -19,6 +19,48 @@
 
 'use strict';
 
+function getContent(str) {
+
+    const escapeReg = /^((?:\\\[)|(?:\\\]))/;
+    let char,
+        count = 1,
+        index = 1,
+        flag = false;
+
+    while (str) {
+
+        if (str.match(escapeReg)) {
+            index += 2;
+            str = str.slice(2);
+            continue;
+        }
+
+        char = str.at(0);
+
+        if (char === '[') {
+            count++;
+        } else if (char === ']') {
+            count--;
+        }
+
+        if (count === 0) {
+            flag = true;
+            break;
+        }
+
+        index++;
+        str = str.slice(1);
+
+    }
+
+    if (!flag) {
+        return;
+    }
+
+    return index;
+
+}
+
 function parse(str, children, renderTree) {
 
     let result = str.match(/^\[(.*?)\][ \t]*\([ \t]*([^")]*?)(?:[ \t]+(["'])(.*?)\3)?[ \t]*\)/);
@@ -42,18 +84,59 @@ function parse(str, children, renderTree) {
 
     }
 
-    result = str.match(/^\[(.*)\][ \t]*\[(.*?)\]/);
+    const reg = /^\[(.*?)\][ \t]*\[(.*?)\]/;
+    result = str.match(reg);
 
     // use a reference define
     if (result) {
 
-        let ref = result[2];
-        if (!result[2]) {
-            ref = result[1];
+        let rawValue, ref, length = 0;
+
+        // if there is a image in a link, like this:
+        // [![alt text][/path/to/img.jpg]][/path/to/link]
+        if (result[1].includes('[')) {
+
+            let tempStr = str.slice(1),
+                index = getContent(tempStr);
+            if (index === undefined) {// link syntax error
+                return;
+            }
+
+            rawValue = str.slice(1, index);
+            tempStr = str.slice(index + 1);
+            length += index + 1;
+
+
+            result = tempStr.match(/^([ \t]*)(\[(?:.*?)\])/);
+            if (!result) {// link syntax error
+                return;
+            }
+
+            length += result[1].length;
+            index = getContent(result[2].slice(1));
+            if (index === undefined) { // link syntax error
+                return;
+            }
+
+            ref = result[2].slice(1, index);
+            length += index + 1;
+
+        } else {
+
+            rawValue = result[1];
+
+            ref = result[2];
+            if (!result[2]) {
+                ref = result[1];
+            }
+
+            ref = ref.toLowerCase();
+
+            length = result[0].length;
+
         }
 
-        ref = ref.toLowerCase();
-
+        // no referenceDefine  matched
         if (!(ref in renderTree.referenceDefine) || !renderTree.referenceDefine[ref].href) {
             return;
         }
@@ -63,7 +146,7 @@ function parse(str, children, renderTree) {
         const node = {
             type: 'Link',
             href: ref.href,
-            rawValue: result[1]
+            rawValue
         };
 
         if (ref.title) {
@@ -72,9 +155,11 @@ function parse(str, children, renderTree) {
 
         this.parseInline(node);
 
-        return [node, result[0].length];
+        return [node, length];
 
     }
+
+    return;
 
 }
 
