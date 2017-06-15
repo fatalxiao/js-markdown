dce5d952efebae0f312870b5ccb6e215033bcb31
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-15 15:36:55 +0800

    update Blockquote test

--- test/Blockquote.test.js
+++ test/Blockquote.test.js
@@ -80,4 +80,28 @@ describe('Blockquote Test', () => {
 
     });
 
+    it('complicated nested', () => {
+
+        const md = '> ## This is a header.\n'
+                + '>\n'
+                + '> 1.   This is the first list item.\n'
+                + '> 2.   This is the second list item.\n'
+                + '>\n'
+                + '> Here\'s some example code:\n'
+                + '>\n'
+                + '>     return shell_exec("echo $input | $markdown_script");',
+            result = '<blockquote>'
+                + '<h2>This is a header.</h2>'
+                + '<ol>'
+                + '<li>This is the first list item.</li>'
+                + '<li>This is the second list item.</li>'
+                + '</ol>'
+                + '<p>Here\'s some example code:</p>'
+                + '<pre><code>return shell_exec(&quot;echo $input | $markdown_script&quot;);\n</code></pre>'
+                + '</blockquote>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
 });
