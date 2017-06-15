8d96d9a60666d41e0880eae28d1e429ea1871f18
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-15 17:29:05 +0800

    add SetextHeader test

---
+++ test/SetextHeader.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import Markdown from '../src';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('AtxHeader Test', () => {
+
+    it('level 1', () => {
+
+        const md = 'Header 1\n'
+                + '========',
+            result = '<h1>Header 1</h1>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('level 2', () => {
+
+        const md = 'Header 2\n'
+                + '--------',
+            result = '<h2>Header 2</h2>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('with paragraph', () => {
+
+        const md = 'Header 2\n'
+                + '--------'
+                + '\n'
+                + 'Here is a paragraph.',
+            result = '<h2>Header 2</h2><p>Here is a paragraph.</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+});
