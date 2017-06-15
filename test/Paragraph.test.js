d5552355410b6c3f50a7ea080c8730998cb61739
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-15 17:25:44 +0800

    add Paragraph test

---
+++ test/Paragraph.test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import Markdown from '../src';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Paragraph Test', () => {
+
+    it('default', () => {
+
+        const md = 'Here\n'
+                + 'is\n'
+                + 'a\n'
+                + 'paragraph.',
+            result = '<p>Here\nis\na\nparagraph.</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('multi lines', () => {
+
+        const md = 'line 1  \n'
+                + 'line 2  \n'
+                + 'line 3',
+            result = '<p>line 1<br/>line 2<br/>line 3</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('several paragraph', () => {
+
+        const md = 'Paragraph 1.  \n'
+                + '\n'
+                + 'Paragraph 2.  \n'
+                + '\n'
+                + 'Paragraph 3.',
+            result = '<p>Paragraph 1.</p><p>Paragraph 2.</p><p>Paragraph 3.</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+});
