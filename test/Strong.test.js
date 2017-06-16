7eb471536ecc02a9e0909c2510204dc0515da0f4
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-16 09:25:47 +0800

    add Strong test

---
+++ test/Strong.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import Markdown from '../src';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Strong Test', () => {
+
+    it('default', () => {
+
+        const md = 'aaa**Strong**bbb   **Strong**   ccc',
+            result = '<p>aaa<strong>Strong</strong>bbb   <strong>Strong</strong>   ccc</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('with Emphasize', () => {
+
+        const md = 'aaa **Strong** bbb ***Emphasize and Strong*** ccc',
+            result = '<p>aaa <strong>Strong</strong> bbb <strong><em>Emphasize and Strong</em></strong> ccc</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+});
