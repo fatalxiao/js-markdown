48af9e2c1bb381844bd3b696d4c8366f65b61a0d
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-06-16 08:36:39 +0800

    add AutoLink test

---
+++ test/AutoLink.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import Markdown from '../src';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('AutoLink Test', () => {
+
+    it('default', () => {
+
+        const md = '<https://github.com/fatalxiao/js-markdown>',
+            result = '<p>'
+                + '<a href="https://github.com/fatalxiao/js-markdown">'
+                + 'https://github.com/fatalxiao/js-markdown'
+                + '</a>'
+                + '</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('invalid url', () => {
+
+        const md = '<github.com/fatalxiao/js-markdown>',
+            result = '<p><github.com/fatalxiao/js-markdown></p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+});
