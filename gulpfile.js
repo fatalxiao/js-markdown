e283664b2df70b633cd86d32503005bc469c84cb
liangxiaojun <fatalxiao@163.com>
2018-04-25 15:05:46 +0800

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,20 @@
-process.env.NODE_ENV = '"release"';
-
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     babel = require('gulp-babel'),
     gulpSequence = require('gulp-sequence'),
     miniPackageJson = require('./scripts/gulp-mini-package-json');
 
-/**
- * es compile
- */
-gulp.task('es', function () {
-    return gulp.src('./src/**/*.js')
-        .pipe(babel({
-            plugins: ['transform-runtime']
-        }))
-        .on('error', function () {
-            console.error(e.toString());
-        })
-        .pipe(gulp.dest('./dist'));
-});
+gulp.task('copyES', () => gulp.src('./src/**/*.js')
+    .pipe(babel({plugins: ['transform-runtime']})).pipe(gulp.dest('./dist'))
+);
 
 /**
  * copy extra files to dist
  */
-gulp.task('copyNpmFiles', function () {
-    return gulp.src(['README.md', './LICENSE'])
-        .pipe(gulp.dest('./dist'));
-});
-gulp.task('copyPackageJson', function () {
-    return gulp.src('./package.json')
-        .pipe(miniPackageJson())
-        .pipe(gulp.dest('./dist'));
-});
+gulp.task('copyNpmFiles', () => gulp.src(['README.md', './LICENSE']).pipe(gulp.dest('./dist')));
+gulp.task('copyPackageJson', () => gulp.src('./package.json').pipe(miniPackageJson()).pipe(gulp.dest('./dist')));
 gulp.task('copyFiles', gulpSequence('copyNpmFiles', 'copyPackageJson'));
 
 /**
  * build components for npm publish
  */
-gulp.task('build', gulpSequence('es', 'copyFiles'));
-
-/**
- * watch components src files
- */
-gulp.task('watch', function () {
-    gulp.watch('./src/**/*.js', ['es']);
-});
+gulp.task('copy', gulpSequence('copyES', 'copyFiles'));
--- scripts/gulp-mini-package-json.js
+++ scripts/gulp-mini-package-json.js
@@ -1,46 +1,43 @@
 'use strict';
 
-var gutil = require('gulp-util'),
+const gutil = require('gulp-util'),
     through = require('through2');
 
-module.exports = function () {
-    return through.obj(function (chunk, encoding, callback) {
+module.exports = () => through.obj(function (chunk, encoding, callback) {
 
-        if (chunk.isNull()) {
-            callback(null, chunk);
-            return;
-        }
+    if (chunk.isNull()) {
+        callback(null, chunk);
+        return;
+    }
 
-        if (chunk.isStream()) {
-            callback(new gutil.PluginError('gulp-mini-package-json', 'Streaming not supported'));
-            return;
-        }
+    if (chunk.isStream()) {
+        callback(new gutil.PluginError('gulp-mini-package-json', 'Streaming not supported'));
+        return;
+    }
 
-        try {
+    try {
 
-            var data = JSON.parse(chunk.contents.toString());
+        const data = JSON.parse(chunk.contents.toString()),
 
-            var miniData = {
+            miniData = {
                 name: data.name,
                 author: data.author,
                 version: data.version,
                 description: data.description,
-                main: './index.js',
                 keywords: data.keywords,
                 repository: data.repository,
                 license: data.license,
                 homepage: data.homepage
             };
 
-            chunk.contents = new Buffer(JSON.stringify(miniData, null, 2));
+        chunk.contents = new Buffer(JSON.stringify(miniData, null, 2));
 
-            this.push(chunk);
+        this.push(chunk);
 
-        } catch (err) {
-            this.emit('error', err);
-        }
+    } catch (e) {
+        this.emit('error', e);
+    }
 
-        callback();
+    callback();
 
-    });
-};
+});
