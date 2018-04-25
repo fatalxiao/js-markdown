775d61293294420d5b512c0e2974c94638e2de22
liangxiaojun <fatalxiao@163.com>
2018-04-25 15:38:49 +0800

    update example

--- examples/index.js
+++ examples/index.js
@@ -3,11 +3,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import AppRoot from 'containers/AppRoot';
-
-injectTapEventPlugin();
 
 render(
     <AppRoot/>,
