c9d6502d60593a537c8842299d4e100f8a8044dd
liangxiaojun <fatalxiao@163.com>
2018-04-25 14:56:26 +0800

    update AppRoot

--- examples/containers/AppRoot.js
+++ examples/containers/AppRoot.js
@@ -2,10 +2,7 @@ import React, {Component} from 'react';
 
 import MarkDownEditor from './MarkDownEditor';
 
-import 'assets/font-awesome/css/font-awesome.min.css';
 import 'sass/global.scss';
-import 'sass/Root.scss';
-import 'sass/example.scss';
 
 export default class AppRoot extends Component {
 
