95b512616041a52c1159b48cc2f714d9fc474c96
liangxiaojun <fatalxiao@163.com>
2018-05-04 15:45:50 +0800

    update Emphasize

--- src/lib/syntax/inline/Emphasize.js
+++ src/lib/syntax/inline/Emphasize.js
@@ -17,60 +17,87 @@
 
 function parse(str, children, renderTree) {
 
+    // const flag = str.at(0),
+    //     reg = new RegExp(`([\\s\\S]*?)(\\${flag})`);
+    //
+    // let restStr = str.slice(1);
+    //
+    // if (!restStr.includes(flag)) {
+    //     return;
+    // }
+    //
+    // let count = 0,
+    //     pn = 1,
+    //     resultStr = '',
+    //     result;
+    //
+    // // find the closing identifier
+    // while (restStr.length > 0) {
+    //
+    //     result = restStr.match(reg);
+    //
+    //     if (!result) {
+    //         break;
+    //     }
+    //
+    //     if (result[1]) {
+    //         restStr = restStr.slice(result[1].length);
+    //         resultStr += result[1];
+    //         continue;
+    //     }
+    //
+    //     if (count === 0 && result[2]) {
+    //         break;
+    //     }
+    //
+    //     // if there is a Strong syntax
+    //     if (restStr.startsWith(flag + flag)) {
+    //         restStr = restStr.slice(2);
+    //         resultStr += flag + flag;
+    //         count += pn;
+    //         pn = -pn;
+    //     }
+    //
+    // }
+    //
+    // if (resultStr.length > 0) {
+    //
+    //     const node = { // emphasize root node
+    //         type: 'Emphasize',
+    //         rawValue: resultStr
+    //     };
+    //
+    //     // parse recursively
+    //     this.parseInline(node);
+    //
+    //     return [node, resultStr.length + 2];
+    //
+    // }
+    //
+    // return;
+
     const flag = str.at(0),
-        reg = new RegExp(`([\\s\\S]*?)(\\${flag})`);
+        reg = new RegExp(`([\\s\\S]*?)(\\${flag})`),
 
-    let restStr = str.slice(1);
-
-    if (!restStr.includes(flag)) {
-        return;
-    }
-
-    let count = 0,
-        pn = 1,
-        resultStr = '',
-        result;
-
-    // find the closing identifier
-    while (restStr.length > 0) {
+        restStr = str.slice(1),
 
         result = restStr.match(reg);
 
-        if (!result) {
-            break;
-        }
-
-        if (result[1]) {
-            restStr = restStr.slice(result[1].length);
-            resultStr += result[1];
-            continue;
-        }
-
-        if (count === 0 && result[2]) {
-            break;
-        }
-
-        // if there is a Strong syntax
-        if (restStr.startsWith(flag + flag)) {
-            restStr = restStr.slice(2);
-            resultStr += flag + flag;
-            count += pn;
-            pn = -pn;
-        }
-
+    if (!result) {
+        return;
     }
 
-    if (resultStr.length > 0) {
+    if (result[1].length > 0) {
 
-        const node = { // emphasize root node
+        const node = { // strong root node
             type: 'Emphasize',
-            rawValue: resultStr
+            rawValue: result[1]
         };
 
         // parse recursively
         this.parseInline(node);
 
-        return [node, resultStr.length + 2];
+        return [node, result[1].length + 2];
 
     }
 
