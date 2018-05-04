d587fc12c473d8a22f7fc0738bbb47a8565ef1c8
liangxiaojun <fatalxiao@163.com>
2018-05-04 15:45:34 +0800

    update Strong

--- src/lib/syntax/inline/Strong.js
+++ src/lib/syntax/inline/Strong.js
@@ -18,59 +18,27 @@
 function parse(str, children, renderTree) {
 
     const flag = str.at(0),
-        reg = new RegExp(`([\\s\\S]*?)(\\${flag})`);
+        reg = new RegExp(`([\\s\\S]*?)(\\${flag}\\${flag})`),
 
-    let restStr = str.slice(2);
-
-    if (!restStr.includes(flag + flag)) {
-        return;
-    }
-
-    let count = 0,
-        pn = 1,
-        resultStr = '',
-        result;
-
-    // find the closing identifier
-    while (restStr.length > 0) {
-
-        // find closing identifier
-        if (count === 0 && restStr.startsWith(flag + flag)) {
-            break;
-        }
+        restStr = str.slice(2),
 
         result = restStr.match(reg);
 
-        if (!result) {
-            break;
-        }
-
-        if (result[1]) {
-            restStr = restStr.slice(result[1].length);
-            resultStr += result[1];
-            continue;
-        }
-
-        if (result[2]) {
-            restStr = restStr.slice(result[2].length);
-            resultStr += result[2];
-            count += pn;
-            pn = -pn;
-        }
-
+    if (!result) {
+        return;
     }
 
-    if (resultStr.length > 0) {
+    if (result[1].length > 0) {
 
         const node = { // strong root node
             type: 'Strong',
-            rawValue: resultStr
+            rawValue: result[1]
         };
 
         // parse recursively
         this.parseInline(node);
 
-        return [node, resultStr.length + 4];
+        return [node, result[1].length + 4];
 
     }
 
