00983627d537cc4d61fa3ceaf614aa7220fc1ac9
liangxiaojun <fatalxiao@163.com>
2018-05-04 10:15:02 +0800

    update SetextHeader

--- src/lib/syntax/block/SetextHeader.js
+++ src/lib/syntax/block/SetextHeader.js
@@ -55,7 +55,7 @@ function render(data = '', node, renderTree) {
 
     }
 
-    return `<h${level}>${innerHtml}</h${level}>`;
+    return `<a id="${innerHtml}" href="#${innerHtml}"><h${level}>${innerHtml}</h${level}></a>`;
 
 }
 
