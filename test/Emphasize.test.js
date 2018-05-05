ec2a42c1750e19b4e02952f9bdef82010529d171
liangxiaojun <fatalxiao@163.com>
2018-05-05 16:36:47 +0800

    update Emphasize.test

--- test/Emphasize.test.js
+++ test/Emphasize.test.js
@@ -7,19 +7,37 @@ const expect = chai.expect;
 
 describe('Emphasize Test', () => {
 
-    it('default', () => {
+    it('weak', () => {
 
-        const md = 'aaa*Emphasize*bbb   *Emphasize*   ccc',
+        const md = 'aaa*Weak Emphasize*bbb   *Weak Emphasize*   ccc',
             result = '<p>aaa<em>Emphasize</em>bbb   <em>Emphasize</em>   ccc</p>';
 
         expect(Markdown.parse(md)).to.be.equal(result);
 
     });
 
-    it('with Strong', () => {
+    it('strong', () => {
 
-        const md = 'aaa *Emphasize* bbb ***Emphasize and Strong*** ccc',
+        const md = 'aaa**Strong Emphasize**bbb   **Strong Emphasize**   ccc',
+            result = '<p>aaa<strong>Strong</strong>bbb   <strong>Strong</strong>   ccc</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('weak and strong', () => {
+
+        const md = 'aaa *Weak Emphasize* bbb ***Weak and Strong Emphasize*** ccc',
             result = '<p>aaa <em>Emphasize</em> bbb <strong><em>Emphasize and Strong</em></strong> ccc</p>';
+
+        expect(Markdown.parse(md)).to.be.equal(result);
+
+    });
+
+    it('with escape character', () => {
+
+        const md = '**\*Strong**',
+            result = '<p>*<strong>Strong</strong></p>';
 
         expect(Markdown.parse(md)).to.be.equal(result);
 
