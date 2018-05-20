75253d4d777c48bc74867fcb20d234e0a9dd91fc
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:43:37 +0800

    update Paragraph test

--- test/Paragraph.test.js
+++ test/Paragraph.test.js
@@ -7,40 +7,37 @@ const expect = chai.expect;
 
 describe('Paragraph Test', () => {
 
-    it('default', () => {
+    it('default', () =>
+        expect(Markdown.parse(
+            'Here\n'
+            + 'is\n'
+            + 'a\n'
+            + 'paragraph.'
+        )).to.be.equal(
+            '<p>Here\nis\na\nparagraph.</p>'
+        )
+    );
 
-        const md = 'Here\n'
-                + 'is\n'
-                + 'a\n'
-                + 'paragraph.',
-            result = '<p>Here\nis\na\nparagraph.</p>';
+    it('multi lines', () =>
+        expect(Markdown.parse(
+            'line 1  \n'
+            + 'line 2  \n'
+            + 'line 3'
+        )).to.be.equal(
+            '<p>line 1<br/>line 2<br/>line 3</p>'
+        )
+    );
 
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('multi lines', () => {
-
-        const md = 'line 1  \n'
-                + 'line 2  \n'
-                + 'line 3',
-            result = '<p>line 1<br/>line 2<br/>line 3</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('several paragraph', () => {
-
-        const md = 'Paragraph 1.  \n'
-                + '\n'
-                + 'Paragraph 2.  \n'
-                + '\n'
-                + 'Paragraph 3.',
-            result = '<p>Paragraph 1.</p><p>Paragraph 2.</p><p>Paragraph 3.</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
+    it('several paragraph', () =>
+        expect(Markdown.parse(
+            'Paragraph 1.  \n'
+            + '\n'
+            + 'Paragraph 2.  \n'
+            + '\n'
+            + 'Paragraph 3.'
+        )).to.be.equal(
+            '<p>Paragraph 1.</p><p>Paragraph 2.</p><p>Paragraph 3.</p>'
+        )
+    );
 
 });
