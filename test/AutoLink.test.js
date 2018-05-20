96f65b4b4b9f24a87b33dca6661b074ac73cbe63
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:26:02 +0800

    update AutoLink test

--- test/AutoLink.test.js
+++ test/AutoLink.test.js
@@ -7,26 +7,24 @@ const expect = chai.expect;
 
 describe('AutoLink Test', () => {
 
-    it('default', () => {
+    it('default', () =>
+        expect(Markdown.parse(
+            '<https://github.com/fatalxiao/js-markdown>'
+        )).to.be.equal(
+            '<p>'
+            + '<a href="https://github.com/fatalxiao/js-markdown">'
+            + 'https://github.com/fatalxiao/js-markdown'
+            + '</a>'
+            + '</p>'
+        )
+    );
 
-        const md = '<https://github.com/fatalxiao/js-markdown>',
-            result = '<p>'
-                + '<a href="https://github.com/fatalxiao/js-markdown">'
-                + 'https://github.com/fatalxiao/js-markdown'
-                + '</a>'
-                + '</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('invalid url', () => {
-
-        const md = '<github.com/fatalxiao/js-markdown>',
-            result = '<p><github.com/fatalxiao/js-markdown></p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
+    it('invalid url', () =>
+        expect(Markdown.parse(
+            '<github.com/fatalxiao/js-markdown>'
+        )).to.be.equal(
+            '<p><github.com/fatalxiao/js-markdown></p>'
+        )
+    );
 
 });
