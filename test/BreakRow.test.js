a583cbf029ca6de81ca30f159d8bb7f6f707e497
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:30:42 +0800

    update BreakRow test

--- test/BreakRow.test.js
+++ test/BreakRow.test.js
@@ -7,23 +7,22 @@ const expect = chai.expect;
 
 describe('BreakRow Test', () => {
 
-    it('default', () => {
-
-        const md = 'line 1  \n'
-                + 'line 2  \n'
-                + 'line 3\n'
-                + 'continou line 3',
-            result = '<p>'
-                + 'line 1'
-                + '<br/>'
-                + 'line 2'
-                + '<br/>'
-                + 'line 3\n'
-                + 'continou line 3'
-                + '</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
+    it('default', () =>
+        expect(Markdown.parse(
+            'line 1  \n'
+            + 'line 2  \n'
+            + 'line 3\n'
+            + 'continou line 3'
+        )).to.be.equal(
+            '<p>'
+            + 'line 1'
+            + '<br/>'
+            + 'line 2'
+            + '<br/>'
+            + 'line 3\n'
+            + 'continou line 3'
+            + '</p>'
+        )
+    );
 
 });
