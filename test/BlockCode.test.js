0b642d635016f77edfcdf9ab2442ec6a53cc393d
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:28:32 +0800

    update BlockCode test

--- test/BlockCode.test.js
+++ test/BlockCode.test.js
@@ -7,43 +7,43 @@ const expect = chai.expect;
 
 describe('BlockCode Test', () => {
 
-    it('default', () => {
-
-        const md = '```\n'
+    it('default', () =>
+        expect(Markdown.parse(
+            '```\n'
             + 'here\n'
             + 'is\n'
             + 'the\n'
             + 'code\n'
-            + '```';
+            + '```'
+        )).to.be.equal(
+            '<pre><code>here\nis\nthe\ncode</code></pre>'
+        )
+    );
 
-        expect(Markdown.parse(md)).to.be.equal('<pre><code>here\nis\nthe\ncode</code></pre>');
-
-    });
-
-    it('with language', () => {
-
-        const md = '```js\n'
+    it('with language', () =>
+        expect(Markdown.parse(
+            '```js\n'
             + 'here\n'
             + 'is\n'
             + 'the\n'
             + 'code\n'
-            + '```';
+            + '```'
+        )).to.be.equal(
+            '<pre><code lang="js">here\nis\nthe\ncode</code></pre>'
+        )
+    );
 
-        expect(Markdown.parse(md)).to.be.equal('<pre><code lang="js">here\nis\nthe\ncode</code></pre>');
-
-    });
-
-    it('meet InlineCode', () => {
-
-        const md = '```js```\n'
+    it('meet InlineCode', () =>
+        expect(Markdown.parse(
+            '```js```\n'
             + 'here\n'
             + 'is\n'
             + 'the\n'
             + 'code\n'
-            + '```';
-
-        expect(Markdown.parse(md)).to.be.equal('<p><code>js</code>\nhere\nis\nthe\ncode</p><pre><code></code></pre>');
-
-    });
+            + '```'
+        )).to.be.equal(
+            '<p><code>js</code>\nhere\nis\nthe\ncode</p><pre><code></code></pre>'
+        )
+    );
 
 });
