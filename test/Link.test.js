920c8fba6faef6cba748007d0865767d0d223d20
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:42:05 +0800

    update Link test

--- test/Link.test.js
+++ test/Link.test.js
@@ -7,59 +7,55 @@ const expect = chai.expect;
 
 describe('Link Test', () => {
 
-    it('default', () => {
+    it('default', () =>
+        expect(Markdown.parse(
+            '[NPM Version](https://npmjs.org/package/js-markdown)'
+        )).to.be.equal(
+            '<p>'
+            + '<a href="https://npmjs.org/package/js-markdown">'
+            + 'NPM Version'
+            + '</a>'
+            + '</p>'
+        )
+    );
 
-        const md = '[NPM Version](https://npmjs.org/package/js-markdown)',
-            result = '<p>'
-                + '<a href="https://npmjs.org/package/js-markdown">'
-                + 'NPM Version'
-                + '</a>'
-                + '</p>';
+    it('with title', () =>
+        expect(Markdown.parse(
+            '[NPM Version](https://npmjs.org/package/js-markdown "NPM Version")'
+        )).to.be.equal(
+            '<p>'
+            + '<a href="https://npmjs.org/package/js-markdown" title="NPM Version">'
+            + 'NPM Version'
+            + '</a>'
+            + '</p>'
+        )
+    );
 
-        expect(Markdown.parse(md)).to.be.equal(result);
+    it('with refrence', () =>
+        expect(Markdown.parse(
+            '[NPM Version][npm-url]\n'
+            + '[npm-url]: https://npmjs.org/package/js-markdown'
+        )).to.be.equal(
+            '<p>'
+            + '<a href="https://npmjs.org/package/js-markdown">'
+            + 'NPM Version'
+            + '</a>'
+            + '</p>'
+        )
+    );
 
-    });
-
-    it('with title', () => {
-
-        const md = '[NPM Version](https://npmjs.org/package/js-markdown "NPM Version")',
-            result = '<p>'
-                + '<a href="https://npmjs.org/package/js-markdown" title="NPM Version">'
-                + 'NPM Version'
-                + '</a>'
-                + '</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('with refrence', () => {
-
-        const md = '[NPM Version][npm-url]\n'
-                + '[npm-url]: https://npmjs.org/package/js-markdown',
-            result = '<p>'
-                + '<a href="https://npmjs.org/package/js-markdown">'
-                + 'NPM Version'
-                + '</a>'
-                + '</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('nested with Image', () => {
-
-        const md = '[![NPM Version][npm-image]][npm-url]\n'
-                + '[npm-image]: https://img.shields.io/npm/v/js-markdown.svg\n'
-                + '[npm-url]: https://npmjs.org/package/js-markdown',
-            result = '<p>'
-                + '<a href="https://npmjs.org/package/js-markdown">'
-                + '<img alt="NPM Version" src="https://img.shields.io/npm/v/js-markdown.svg"/>'
-                + '</a>'
-                + '</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
+    it('nested with Image', () =>
+        expect(Markdown.parse(
+            '[![NPM Version][npm-image]][npm-url]\n'
+            + '[npm-image]: https://img.shields.io/npm/v/js-markdown.svg\n'
+            + '[npm-url]: https://npmjs.org/package/js-markdown'
+        )).to.be.equal(
+            '<p>'
+            + '<a href="https://npmjs.org/package/js-markdown">'
+            + '<img alt="NPM Version" src="https://img.shields.io/npm/v/js-markdown.svg"/>'
+            + '</a>'
+            + '</p>'
+        )
+    );
 
 });
