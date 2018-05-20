fcc982d06e93334bf79269d6df41dd8b1c687284
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:19:36 +0800

    update List test

--- test/List.test.js
+++ test/List.test.js
@@ -8,203 +8,212 @@ const expect = chai.expect;
 describe('Unordered List Test', () => {
 
     it('use "*"', () => {
-
-        const md = '* Red\n'
+        expect(Markdown.parse(
+            '* Red\n'
             + '* Green\n'
-            + '* Blue',
-            result = '<ul>'
-                + '<li><p>Red</p></li>'
-                + '<li><p>Green</p></li>'
-                + '<li><p>Blue</p></li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '* Blue'
+        )).to.be.equal(
+            '<ul>'
+            + '<li><p>Red</p></li>'
+            + '<li><p>Green</p></li>'
+            + '<li><p>Blue</p></li>'
+            + '</ul>'
+        );
     });
 
     it('use "+"', () => {
-
-        const md = '+ Red\n'
+        expect(Markdown.parse(
+            '+ Red\n'
             + '+ Green\n'
-            + '+ Blue',
-            result = '<ul>'
-                + '<li><p>Red</p></li>'
-                + '<li><p>Green</p></li>'
-                + '<li><p>Blue</p></li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '+ Blue'
+        )).to.be.equal(
+            '<ul>'
+            + '<li><p>Red</p></li>'
+            + '<li><p>Green</p></li>'
+            + '<li><p>Blue</p></li>'
+            + '</ul>'
+        );
     });
 
     it('use "-"', () => {
-
-        const md = '- Red\n'
+        expect(Markdown.parse(
+            '- Red\n'
             + '- Green\n'
-            + '- Blue',
-            result = '<ul>'
-                + '<li><p>Red</p></li>'
-                + '<li><p>Green</p></li>'
-                + '<li><p>Blue</p></li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '- Blue'
+        )).to.be.equal(
+            '<ul>'
+            + '<li><p>Red</p></li>'
+            + '<li><p>Green</p></li>'
+            + '<li><p>Blue</p></li>'
+            + '</ul>'
+        );
     });
 
     it('omit "*"', () => {
-
-        const md = '* Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
+        expect(Markdown.parse(
+            '* Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
             + '    \n'
             + '    Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
             + '    viverra nec, fringilla in, laoreet vitae, risus.\n'
             + '    \n'
             + '* Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
-            + '    Suspendisse id sem consectetuer libero luctus adipiscing.',
-            result = '<ul>'
-                + '<li>'
-                + '<p>'
-                + 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
-                + '</p>'
-                + '<p>'
-                + 'Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
-                + 'viverra nec, fringilla in, laoreet vitae, risus.'
-                + '</p>'
-                + '</li>'
-                + '<li>'
-                + '<p>'
-                + 'Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
-                + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
-                + '</p>'
-                + '</li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '    Suspendisse id sem consectetuer libero luctus adipiscing.'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>'
+            + 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.'
+            + '</p>'
+            + '<p>'
+            + 'Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
+            + 'viverra nec, fringilla in, laoreet vitae, risus.'
+            + '</p>'
+            + '</li>'
+            + '<li>'
+            + '<p>'
+            + 'Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
+            + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
+            + '</p>'
+            + '</li>'
+            + '</ul>'
+        );
     });
 
     it('omit "*" and indent 1', () => {
-
-        const md = '* Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
+        expect(Markdown.parse(
+            '* Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
             + 'Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
             + 'viverra nec, fringilla in, laoreet vitae, risus.\n'
             + '* Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
-            + 'Suspendisse id sem consectetuer libero luctus adipiscing.',
-            result = '<ul>'
-                + '<li>'
-                + '<p>'
-                + 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
-                + 'Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
-                + 'viverra nec, fringilla in, laoreet vitae, risus.'
-                + '</p>'
-                + '</li>'
-                + '<li>'
-                + '<p>'
-                + 'Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
-                + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
-                + '</p>'
-                + '</li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>'
+            + 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n'
+            + 'Aliquam hendrerit mi posuere lectus. Vestibulum enim wisi,\n'
+            + 'viverra nec, fringilla in, laoreet vitae, risus.'
+            + '</p>'
+            + '</li>'
+            + '<li>'
+            + '<p>'
+            + 'Donec sit amet nisl. Aliquam semper ipsum sit amet velit.\n'
+            + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
+            + '</p>'
+            + '</li>'
+            + '</ul>'
+        );
     });
 
     it('omit "*" and indent 2', () => {
-
-        const md = '* This is a list item with two paragraphs.\n'
+        expect(Markdown.parse(
+            '* This is a list item with two paragraphs.\n'
             + '\n'
             + '    This is the second paragraph in the list item. You\'re\n'
             + 'only required to indent the first line. Lorem ipsum dolor\n'
             + 'sit amet, consectetuer adipiscing elit.\n'
             + '\n'
-            + '*   Another item in the same list.',
-            result = '<ul>'
-                + '<li>'
-                + '<p>This is a list item with two paragraphs.</p>'
-                + '<p>'
-                + 'This is the second paragraph in the list item. You\'re\n'
-                + 'only required to indent the first line. Lorem ipsum dolor\n'
-                + 'sit amet, consectetuer adipiscing elit.'
-                + '</p>'
-                + '</li>'
-                + '<li><p>Another item in the same list.</p></li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '*   Another item in the same list.'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>This is a list item with two paragraphs.</p>'
+            + '<p>'
+            + 'This is the second paragraph in the list item. You\'re\n'
+            + 'only required to indent the first line. Lorem ipsum dolor\n'
+            + 'sit amet, consectetuer adipiscing elit.'
+            + '</p>'
+            + '</li>'
+            + '<li><p>Another item in the same list.</p></li>'
+            + '</ul>'
+        );
     });
 
     it('with blank line', () => {
-
-        const md = '* Red\n'
+        expect(Markdown.parse(
+            '* Red\n'
             + '\n'
             + '* Green\n'
             + '\n'
-            + '* Blue',
-            result = '<ul>'
-                + '<li><p>Red</p></li>'
-                + '<li><p>Green</p></li>'
-                + '<li><p>Blue</p></li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '* Blue'
+        )).to.be.equal(
+            '<ul>'
+            + '<li><p>Red</p></li>'
+            + '<li><p>Green</p></li>'
+            + '<li><p>Blue</p></li>'
+            + '</ul>'
+        );
     });
 
-    it('nested unordered list', () => {
+    // it('nested', () => {
+    //     expect(Markdown.parse(
+    //         '* A list item with a blockquote:\n'
+    //         + '\n'
+    //         + '    * sub list 1\n'
+    //         + '    * sub list 2'
+    //     )).to.be.equal(
+    //         '<ul>'
+    //         + '<li>'
+    //         + '<p>A list item with a blockquote:</p>'
+    //         + '<ul>'
+    //         + '<li><p>sub list 1</p></li>'
+    //         + '<li><p>sub list 2</p></li>'
+    //         + '</ul>'
+    //         + '</li>'
+    //         + '</ul>'
+    //     );
+    // });
 
-        const md = '* A list item with a blockquote:\n'
+    it('nested unordered list', () => {
+        expect(Markdown.parse(
+            '* A list item with a blockquote:\n'
             + '\n'
             + '    * sub list 1\n'
-            + '    * sub list 2',
-            result = '<ul>'
-                + '<li>'
-                + '<p>A list item with a blockquote:</p>'
-                + '<ul>'
-                + '<li><p>sub list 1</p></li>'
-                + '<li><p>sub list 2</p></li>'
-                + '</ul>'
-                + '</li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '    * sub list 2'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>A list item with a blockquote:</p>'
+            + '<ul>'
+            + '<li><p>sub list 1</p></li>'
+            + '<li><p>sub list 2</p></li>'
+            + '</ul>'
+            + '</li>'
+            + '</ul>'
+        );
     });
 
     it('nested blockquote', () => {
-
-        const md = '* A list item with a blockquote:\n'
+        expect(Markdown.parse(
+            '* A list item with a blockquote:\n'
             + '\n'
             + '    > This is a blockquote\n'
-            + '    > inside a list item.',
-            result = '<ul>'
-                + '<li>'
-                + '<p>A list item with a blockquote:</p>'
-                + '<blockquote>'
-                + '<p>This is a blockquote\ninside a list item.</p>'
-                + '</blockquote>'
-                + '</li>'
-                + '</ul>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '    > inside a list item.'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>A list item with a blockquote:</p>'
+            + '<blockquote>'
+            + '<p>This is a blockquote\ninside a list item.</p>'
+            + '</blockquote>'
+            + '</li>'
+            + '</ul>'
+        );
     });
 
     it('break list', () => {
-
-        const md = '* List\n'
+        expect(Markdown.parse(
+            '* List\n'
             + '\n'
-            + 'Paragraph',
-            result = '<ul>'
-                + '<li>'
-                + '<p>List</p>'
-                + '</li>'
-                + '</ul>'
-                + '<p>Paragraph</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + 'Paragraph'
+        )).to.be.equal(
+            '<ul>'
+            + '<li>'
+            + '<p>List</p>'
+            + '</li>'
+            + '</ul>'
+            + '<p>Paragraph</p>'
+        );
     });
 
 });
@@ -212,38 +221,36 @@ describe('Unordered List Test', () => {
 describe('Ordered List Test', () => {
 
     it('default', () => {
-
-        const md = '1. Bird\n'
+        expect(Markdown.parse(
+            '1. Bird\n'
             + '2. McHale\n'
-            + '3. Parish',
-            result = '<ol>'
-                + '<li><p>Bird</p></li>'
-                + '<li><p>McHale</p></li>'
-                + '<li><p>Parish</p></li>'
-                + '</ol>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '3. Parish'
+        )).to.be.equal(
+            '<ol>'
+            + '<li><p>Bird</p></li>'
+            + '<li><p>McHale</p></li>'
+            + '<li><p>Parish</p></li>'
+            + '</ol>'
+        );
     });
 
     it('with wrong number', () => {
-
-        const md = '3. Bird\n'
+        expect(Markdown.parse(
+            '3. Bird\n'
             + '1. McHale\n'
-            + '8. Parish',
-            result = '<ol>'
-                + '<li><p>Bird</p></li>'
-                + '<li><p>McHale</p></li>'
-                + '<li><p>Parish</p></li>'
-                + '</ol>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '8. Parish'
+        )).to.be.equal(
+            '<ol>'
+            + '<li><p>Bird</p></li>'
+            + '<li><p>McHale</p></li>'
+            + '<li><p>Parish</p></li>'
+            + '</ol>'
+        );
     });
 
     it('consist of paragraphs', () => {
-
-        const md = '1. This is a list item with two paragraphs. Lorem ipsum dolor\n'
+        expect(Markdown.parse(
+            '1. This is a list item with two paragraphs. Lorem ipsum dolor\n'
             + '    sit amet, consectetuer adipiscing elit. Aliquam hendrerit\n'
             + '    mi posuere lectus.\n'
             + '\n'
@@ -254,55 +261,53 @@ describe('Ordered List Test', () => {
             + '2. Suspendisse id sem consectetuer libero luctus adipiscing.\n'
             + '\n'
             + '\n'
-            + '3. New Ordered List.',
-            result = '<ol>'
-                + '<li>'
-                + '<p>'
-                + 'This is a list item with two paragraphs. Lorem ipsum dolor\n'
-                + 'sit amet, consectetuer adipiscing elit. Aliquam hendrerit\n'
-                + 'mi posuere lectus.'
-                + '</p>'
-                + '<p>'
-                + 'Vestibulum enim wisi, viverra nec, fringilla in, laoreet\n'
-                + 'vitae, risus. Donec sit amet nisl. Aliquam semper ipsum\n'
-                + 'sit amet velit.'
-                + '</p>'
-                + '</li>'
-                + '<li>'
-                + '<p>'
-                + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
-                + '</p>'
-                + '</li>'
-                + '<li>'
-                + '<p>'
-                + 'New Ordered List.'
-                + '</p>'
-                + '</li>'
-                + '</ol>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '3. New Ordered List.'
+        )).to.be.equal(
+            '<ol>'
+            + '<li>'
+            + '<p>'
+            + 'This is a list item with two paragraphs. Lorem ipsum dolor\n'
+            + 'sit amet, consectetuer adipiscing elit. Aliquam hendrerit\n'
+            + 'mi posuere lectus.'
+            + '</p>'
+            + '<p>'
+            + 'Vestibulum enim wisi, viverra nec, fringilla in, laoreet\n'
+            + 'vitae, risus. Donec sit amet nisl. Aliquam semper ipsum\n'
+            + 'sit amet velit.'
+            + '</p>'
+            + '</li>'
+            + '<li>'
+            + '<p>'
+            + 'Suspendisse id sem consectetuer libero luctus adipiscing.'
+            + '</p>'
+            + '</li>'
+            + '<li>'
+            + '<p>'
+            + 'New Ordered List.'
+            + '</p>'
+            + '</li>'
+            + '</ol>'
+        );
     });
 
     it('with strong emphasize', () => {
-
-        const md = '1. **a**: aaaaaaaaaaa:  \n'
+        expect(Markdown.parse(
+            '1. **a**: aaaaaaaaaaa:  \n'
             + '   ![image](./images/image.png)'
             + '   **a1**: a1.  \n'
-            + '   **a2**: a2.',
-            result = '<ol>'
-                + '<li>'
-                + '<p>'
-                + '<strong>a</strong>: aaaaaaaaaaa:<br/>'
-                + '   <img alt="image" src="./images/image.png"/>'
-                + '   <strong>a1</strong>: a1.<br/>'
-                + '   <strong>a2</strong>: a2.'
-                + '</p>'
-                + '</li>'
-                + '</ol>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+            + '   **a2**: a2.'
+        )).to.be.equal(
+            '<ol>'
+            + '<li>'
+            + '<p>'
+            + '<strong>a</strong>: aaaaaaaaaaa:<br/>'
+            + '   <img alt="image" src="./images/image.png"/>'
+            + '   <strong>a1</strong>: a1.<br/>'
+            + '   <strong>a2</strong>: a2.'
+            + '</p>'
+            + '</li>'
+            + '</ol>'
+        );
     });
 
 });
