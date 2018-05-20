8de694ac6d738289a0b0c96a587e41bfb38fdb58
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:42:38 +0800

    update MetaData test

--- test/MetaData.test.js
+++ test/MetaData.test.js
@@ -8,16 +8,15 @@ const expect = chai.expect;
 describe('MetaData Test', () => {
 
     it('default', () => {
-
-        const md = 'name:        js-markdown\n'
-                + 'author:      falalxiao\n'
-                + 'description: A markdown language js compiler.\n'
-                + '\n'
-                + 'Here is a paragraph.',
-            result = '<p>Here is a paragraph.</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
+        expect(Markdown.parse(
+            'name:        js-markdown\n'
+            + 'author:      falalxiao\n'
+            + 'description: A markdown language js compiler.\n'
+            + '\n'
+            + 'Here is a paragraph.'
+        )).to.be.equal(
+            '<p>Here is a paragraph.</p>'
+        );
     });
 
 });
