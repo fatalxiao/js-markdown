10faaa8636022125fb549906d3560920c0191147
liangxiaojun <fatalxiao@163.com>
2018-05-20 10:44:34 +0800

    update SetextHeader test

--- test/SetextHeader.test.js
+++ test/SetextHeader.test.js
@@ -7,36 +7,33 @@ const expect = chai.expect;
 
 describe('SetextHeader Test', () => {
 
-    it('level 1', () => {
+    it('level 1', () =>
+        expect(Markdown.parse(
+            'Header 1\n'
+            + '========'
+        )).to.be.equal(
+            '<h1><a id="Header 1" href="#Header 1"></a>Header 1</h1>'
+        )
+    );
 
-        const md = 'Header 1\n'
-                + '========',
-            result = '<h1><a id="Header 1" href="#Header 1"></a>Header 1</h1>';
+    it('level 2', () =>
+        expect(Markdown.parse(
+            'Header 2\n'
+            + '--------'
+        )).to.be.equal(
+            '<h2><a id="Header 2" href="#Header 2"></a>Header 2</h2>'
+        )
+    );
 
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('level 2', () => {
-
-        const md = 'Header 2\n'
-                + '--------',
-            result = '<h2><a id="Header 2" href="#Header 2"></a>Header 2</h2>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
-
-    it('with paragraph', () => {
-
-        const md = 'Header 2\n'
-                + '--------'
-                + '\n'
-                + 'Here is a paragraph.',
-            result = '<h2><a id="Header 2" href="#Header 2"></a>Header 2</h2><p>Here is a paragraph.</p>';
-
-        expect(Markdown.parse(md)).to.be.equal(result);
-
-    });
+    it('with paragraph', () =>
+        expect(Markdown.parse(
+            'Header 2\n'
+            + '--------'
+            + '\n'
+            + 'Here is a paragraph.'
+        )).to.be.equal(
+            '<h2><a id="Header 2" href="#Header 2"></a>Header 2</h2><p>Here is a paragraph.</p>'
+        )
+    );
 
 });
