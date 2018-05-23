9da91fd492672a35a5155c00b302ce536fbb867e
liangxiaojun <fatalxiao@163.com>
2018-05-23 10:47:59 +0800

    add MsgBox syntax

---
+++ src/lib/syntax/block/MsgBox.js
@@ -0,0 +1,98 @@
+/**
+ * match a message box
+ *
+ * All text in block will be escaped.
+ *
+ * (1) use 3 or more "`" or "~" for the first line, and add use, syntax like this:
+ *
+ *  ```info
+ *  here is the code
+ *  multi lines
+ *  ```
+ *
+ * or
+ *
+ *  ~~~
+ *  another example
+ *  ~~~
+ *
+ * (2) use a tab or 4 space or 1~3 spaces plus a tab for line start, syntax like this:
+ *
+ *      here is the code
+ *      multi lines
+ *
+ */
+
+'use strict';
+
+import Util from '../../utils/Util';
+import Str from '../../utils/Str';
+
+/**
+ * exclude the condition that if there is a same identifier in the same line
+ * @param line
+ */
+function isInlineMatch(line) {
+    return line.match(/^([`~]{3,}).*\1/);
+}
+
+function parse(line, index, lines, renderTree) {
+
+    let result = line.match(/^([`~]{3,}| {4}|\t| {0,3}\t)\s*((info|warning|success|error)??)(?:\n|$)/);
+
+    if (!result) {
+        return;
+    }
+
+    // exclude inline code
+    if (isInlineMatch(line)) {
+        return;
+    }
+
+    if (result[1].includes('\t') || result[1].includes(' ')) { // space or tab
+
+        const indentLen = result[1].length;
+        let codeContent = [result[2]];
+        index++;
+
+        for (let len = lines.length; index < len; index++) {
+            if (Str.trim(lines[index], ' \t') !== '' && !lines[index].startsWith(result[1])) {
+                break;
+            }
+            codeContent.push(lines[index].slice(indentLen));
+        }
+
+        return [{
+            type: 'MsgBox',
+            rawValue: Str.encodeHTML(Util.trimEndBlankLines(codeContent).join('\n')) + '\n'
+        }, index - 1];
+
+    } else { // ``` or ~~~
+
+        let codeContent = [];
+        index++;
+        for (let len = lines.length; index < len; index++) {
+            if (Str.trimEnd(lines[index], ' \t') === result[1]) {
+                break;
+            }
+            codeContent.push(lines[index]);
+        }
+
+        return [{
+            type: 'MsgBox',
+            language: result[2],
+            rawValue: Str.encodeHTML(Util.trimEndBlankLines(codeContent).join('\n'))
+        }, index];
+
+    }
+
+}
+
+function render(data = '', node) {
+    return `<pre><code${node.language ? ` lang="${node.language}"` : ''}>${node.rawValue || ''}${data}</code></pre>`;
+}
+
+export default {
+    parse,
+    render
+};
