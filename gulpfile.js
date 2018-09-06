b7d06c4237a600ac6f4a708b996111b46ab4abcf
liangxiaojun <fatalxiao@163.com>
2018-09-06 11:03:07 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 const gulp = require('gulp'),
     babel = require('gulp-babel'),
-    gulpSequence = require('gulp-sequence'),
     miniPackageJson = require('./scripts/gulp-mini-package-json');
 
-gulp.task('copyES', () => gulp.src('./src/**/*.js')
-    .pipe(babel({
-        plugins: ['transform-runtime']
-    })).pipe(gulp.dest('./dist'))
+gulp.task('copyES', () =>
+    gulp.src('./src/**/*.js')
+        .pipe(babel({
+            presets: [['@babel/env', {modules: 'commonjs'}]],
+            plugins: ['@babel/plugin-transform-runtime']
+        })).pipe(gulp.dest('./dist'))
 );
 
 /**
  * copy extra files to dist
  */
-gulp.task('copyNpmFiles', () => gulp.src(['README.md', './LICENSE']).pipe(gulp.dest('./dist')));
-gulp.task('copyPackageJson', () => gulp.src('./package.json').pipe(miniPackageJson()).pipe(gulp.dest('./dist')));
-gulp.task('copyFiles', gulpSequence('copyNpmFiles', 'copyPackageJson'));
+gulp.task('copyNpmFiles', () =>
+    gulp.src(['README.md', './LICENSE'])
+        .pipe(gulp.dest('./dist'))
+);
+gulp.task('copyPackageJson', () =>
+    gulp.src('./package.json')
+        .pipe(miniPackageJson())
+        .pipe(gulp.dest('./dist'))
+);
+gulp.task('copyFiles', gulp.series('copyNpmFiles', 'copyPackageJson'));
 
 /**
  * build components for npm publish
  */
-gulp.task('copy', gulpSequence('copyES', 'copyFiles'));
+gulp.task('copy', gulp.series('copyES', 'copyFiles'));
